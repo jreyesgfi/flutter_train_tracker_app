50bbbef5dc01a95a16ff739ce88c1aaec5060346
jorge <91345902+jreyesgfi@users.noreply.github.com>
2024-02-14 00:59:08 +0200

    Amplify Added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,11 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_application_test1/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smWoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
+    // Verify thWat our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
